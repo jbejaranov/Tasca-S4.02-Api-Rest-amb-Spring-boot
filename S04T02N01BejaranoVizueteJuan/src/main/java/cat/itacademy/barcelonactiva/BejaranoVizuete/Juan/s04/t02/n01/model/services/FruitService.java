e508a08105962c13a6b05b343b2bd6fd1c2f5f4b
Juan A. Bejarano <100026204+jbejaranov@users.noreply.github.com>
2022-11-07 12:23:32 +0100

    Update FruitService.java

--- S04T02N01BejaranoVizueteJuan/src/main/java/cat/itacademy/barcelonactiva/BejaranoVizuete/Juan/s04/t02/n01/model/services/FruitService.java
+++ S04T02N01BejaranoVizueteJuan/src/main/java/cat/itacademy/barcelonactiva/BejaranoVizuete/Juan/s04/t02/n01/model/services/FruitService.java
@@ -5,21 +5,7 @@ import java.util.List;
 import cat.itacademy.barcelonactiva.BejaranoVizuete.Juan.s04.t02.n01.model.domain.Fruit;
 
 public interface FruitService {
-/*
-	public List<Fruit> getAll();
 
-	public Fruit getOne(Long id);
-
-	public Fruit addFruit(Fruit fruit);
-
-	public void deleteFruit(Long id);
-
-	public Fruit updateFruit(Fruit fruit);
-
-	*/
-	
-	
-	//
 	List<Fruit> findAllFruits();
 	
 	Fruit findFruitById(Long id);
